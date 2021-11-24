142b6f477775cbddacf70df8bb13212da7223740
Nikita Ryzhkin <timongo@bk.ru>
2021-11-24 12:34:22 +0400

    Storyboard

---
+++ ShowAnimation/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  ShowAnimation
+//
+//  Created by Никита Рыжкин on 24.11.2021.
+//
+
+import Foundation
+
+struct Animation {
+    let preset: String
+    let curve: String
+    let force: Double
+    let duration: Double
+    let delay: Double
+}
--- ShowAnimation/ViewController.swift
+++ ShowAnimation/ViewController.swift
@@ -5,15 +5,25 @@
 //  Created by Никита Рыжкин on 24.11.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet var animatedView: UIView!
+    @IBOutlet var presetLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    @IBOutlet var runAnimationButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func runAnimationButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
